b01405f52f03c1cc1499781ebb8ec63da4522899
glassp <pascal.glaesser1997@gmail.com>
2020-02-02 20:57:40 +0100

    changed from Github API v3 to v4

--- src/projects.js
+++ src/projects.js
@@ -1,32 +1,58 @@
+//DO NOT STORE A TOKEN DIRECTLY LIKE THIS FOR CRITICAL SYSTEMS
+//BEST PRACTICE IS TO NEVER STORE TOKENS IN THE FRONT-END
+//This is a rare case where it is not possible to store it in the backend as Github pages does not allow backend code
+//Therefor if one has to use this approach ensure that the token is read-only for non-critical data
+var token = "965ab1133f437eb8dd7e7102cf2fbfe3201b6cfa"
+
+//send a POST request using jquery to the Github GraphQL API
 $.ajax({
-  url: "https://api.github.com/users/glassp/repos",
-  success: function(result){
-    for(var i in result){
-      var repo = result[i].name;
-      var user = result[i].owner.login;
-      var url = result[i].html_url;
-      var desc = result[i].description;
-      var isFork = result[i].fork;
-      if(!isFork&&repo!=="examples")
+    method: "POST",
+    url: "https://api.github.com/graphql",
+    contentType: "application/json",
+    headers: {
+      //Authorize using the token as anonymous is not implemented yet.
+      Authorization: `bearer ${token}`
+    },
+    //What information to fetch
+    data: JSON.stringify({
+      query: `query {
+          user(login:"glassp"){
+            repositories(first:100){
+              nodes{
+                name,
+                openGraphImageUrl,
+                url,
+                description
+                isFork,
+                owner{
+                  login
+                }
+              }
+            }
+          }
+        }`
+    }),
+    success: function(result) {
+      //parse information into variable repo
+      var repos = result.data.user.repositories.nodes;
+      for(var i in repos){
+        var repo = repos[i];
+        if(!repo.isFork&& !repo.name.includes("examples"))
+        //inject a html card with the repo info (exclude forked and examples repo)
         $("#projects").append(`
           <div class="card">
-            <img class="card-img-top" src="res/placeholder.jpg" alt="Card image cap">
+            <img class="card-img-top" src="${repo.openGraphImageUrl}" alt="Card image cap">
             <div class="card-body">
-              <h5 class="card-title">${repo}</h5>
+              <h5 class="card-title">${repo.name}</h5>
               <p class="card-text">
-                <img alt="GitHub top language" src="https://img.shields.io/github/languages/top/${user}/${repo}">
-                <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/${user}/${repo}">
+                <img alt="GitHub top language" src="https://img.shields.io/github/languages/top/${repo.owner.login}/${repo.name}">
+                <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/${repo.owner.login}/${repo.name}">
               </p>
-              <p class="card-text">${desc}</p>
-              <a href="${url}" class="btn btn-primary">Go to Project</a>
+              <p class="card-text">${repo.description}</p>
+              <a href="${repo.url}" class="btn btn-primary">Go to Project</a>
             </div>
           </div>
         `);
+      }
     }
-  }
-})
-/*
-
-
-
-*/
+  });
