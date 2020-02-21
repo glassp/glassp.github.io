d736c6ddfb269605c2826f6339832cff8bd613af
glassp <pascal.glaesser1997@gmail.com>
2020-02-21 17:00:55 +0100

    SCRIPT-UPLOAD: commiting latest version to master

--- upload.sh
+++ upload.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-cd ./hidden/schedule
-rm master.sched
-touch master.sched
-for f in *.schedule; do
-  echo "$(cat $f)" >> master.sched;
-done
-cd ../..
 git add .
 git commit -m "SCRIPT-UPLOAD: commiting latest version to master"
 git push origin master
